5d8708e4dbac63a5d143a79d6c44ea235542eafa
Dawid Czartoszewski <dawid.czartoszewski@gmail.com>
2018-07-10 15:37:19 +0200

    add new feature

--- src/main/java/pl/akademiakodu/AK_Spring_ExerciseMethod_BasicCalculatorTODO/controller/DescController.java
+++ src/main/java/pl/akademiakodu/AK_Spring_ExerciseMethod_BasicCalculatorTODO/controller/DescController.java
@@ -7,7 +7,9 @@ package pl.akademiakodu.AK_Spring_ExerciseMethod_BasicCalculatorTODO.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import pl.akademiakodu.AK_Spring_ExerciseMethod_BasicCalculatorTODO.models.Person;
 
 import java.util.Random;
 import java.util.Set;
@@ -41,5 +43,18 @@ public class DescController {
     /*
     szuka pliku random.html, który musi znajdować się w katalogu templates
      */
+    @GetMapping("/form")
+    public String getForm(){
+    return  "form";
+}
+    @GetMapping("/result")
+    public String result(@RequestParam
+                        String firstName,
+                         @RequestParam String lastName
+                        ,ModelMap modelMap){
+        Person person = new Person(firstName,lastName);
+        modelMap.put("person",person);
+        return "person";
+    }
 
 }
